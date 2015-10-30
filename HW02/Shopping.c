b5f83c47d30e01c85c6d2f541f8d7750b6924af7
Mark <mluzarow@users.noreply.github.com>
2015-10-30 14:16:25 -0500

    Fixed file

--- HW02/Shopping.c
+++ HW02/Shopping.c
@@ -6,11 +6,11 @@ int main() {
     int remote = 0;     //Stores amount of remote controls.
     int mp3_player = 0; //Stores amount of MP3 players.
     int laptop = 0;     //Stores amount of laptops.
-    float TV = 599.99;         //Sets price of television.
-    float BR_PLAYER = 199.99;  //Sets price of Blu-ray player.
-    float REMOTE = 29.99;      //Sets price of remote control.
-    float MP3_PLAYER = 44.99;  //Sets price of MP3 player.
-    float LAPTOP = 349.99;     //Sets price of laptop.
+    const float TV = 599.99;         //Sets price of television.
+    const float BR_PLAYER = 199.99;  //Sets price of Blu-ray player.
+    const float REMOTE = 29.99;      //Sets price of remote control.
+    const float MP3_PLAYER = 44.99;  //Sets price of MP3 player.
+    const float LAPTOP = 349.99;     //Sets price of laptop.
 
     //Gathers the amount of televisions the user wishes to buy.
     printf("\nEnter the quantity of televisions: ");
@@ -20,6 +20,9 @@ int main() {
     scanf("%d", &br_player);
     //Gathers the amount of remote controls the user wishes to buy.
     printf("Enter the quantity of remote controls: ");
+    scanf("%d", &remote);
+    //Gathers the amount of MP3 players the user wishes to buy.
+    printf("Enter the quantity of MP3 players: ");
     scanf("%d", &mp3_player);
     //Gathers the amount of laptops the user wishes to buy.
     printf("Enter the quantity of laptops: ");
@@ -30,11 +33,11 @@ int main() {
     printf("\n---  -----------      ----------  -----------");
 
     //Prints quantities and their prices according to the frame.
-    printf("\n%3.0f  Televisions      $%9.2f  $%10.2f", (float)  tv, TV, TV * tv);
-    printf("\n%3.0f  Blu-ray Players  $%9.2f  $%10.2f", (float) br_player, BR_PLAYER, BR_PLAYER * br_player);
-    printf("\n%3.0f  Remote Controls  $%9.2f  $%10.2f", (float) remote, REMOTE, REMOTE * remote);
-    printf("\n%3.0f  MP3 Players      $%9.2f  $%10.2f", (float) mp3_player, MP3_PLAYER, MP3_PLAYER * mp3_player);
-    printf("\n%3.0f  Laptop Computers $%9.2f  $%10.2f", (float) laptop, LAPTOP, LAPTOP * laptop);
+    printf("\n%3d  Television%c      $%9.2f  $%10.2f", tv, tv == 1 ? ' ' : 's', TV, TV * tv);
+    printf("\n%3d  Blu-ray Player%c  $%9.2f  $%10.2f", br_player, br_player == 1 ? ' ' : 's', BR_PLAYER, BR_PLAYER * br_player);
+    printf("\n%3d  Remote Control%c  $%9.2f  $%10.2f", remote, remote == 1 ? ' ' : 's', REMOTE, REMOTE * remote);
+    printf("\n%3d  MP3 Player%c      $%9.2f  $%10.2f", mp3_player, mp3_player == 1 ? ' ' : 's', MP3_PLAYER, MP3_PLAYER * mp3_player);
+    printf("\n%3d  Laptop Computer%c $%9.2f  $%10.2f", laptop, laptop == 1 ? ' ' : 's', LAPTOP, LAPTOP * laptop);
 
     //Prints part of the text frame.
     printf("\n                                  -----------");
@@ -44,7 +47,6 @@ int main() {
     printf("\n                             Tax  $%10.2f", (BR_PLAYER*br_player + REMOTE*remote + MP3_PLAYER*mp3_player + LAPTOP*laptop) * .07);
     printf("\n                           Total  $%10.2f", (BR_PLAYER*br_player + REMOTE*remote + MP3_PLAYER*mp3_player + LAPTOP*laptop) * 1.07);
     printf("\n");
-
 
     return(0);
 } //main
